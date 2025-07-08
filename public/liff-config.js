56e26da30010a232b02435629f7efd7f997fb007
ธนารตน สวรรณนนท <out-thanaratsuw@counterservice.co.th>
2025-07-08 14:57:36 +0700

    update liff

--- public/liff-config.js
+++ public/liff-config.js
@@ -9,7 +9,7 @@ const DOMAIN = "https://test.nhamaew.net/";
 const URL_UPDATE_ANIMAL_TYPE = DOMAIN+"api/v1/cat-bot/update-animal-type";
 
 var profileTest = {
-    userId:'U88ca6af84181b2f92c62f50ab4b5da52',
+    userId:'U88ca6af84181b2f92c62f50ab4b5da523',
     displayName:'Tent365💰💰',
     statusMessage:'อย่าพยายาม ทำในสิ่งที่เป็นไปไม่ได้',
     pictureUrl:'https://profile.line-scdn.net/0h3-mBgel0bAJAO3l34VQSfTBrb2hjSjUQPw0jNnNoYWZ9CX8DaQoqMCY7MmUpDC9ROw5xYHE6YWFMKBtkXm2QNkcLMTN8CCtXa18i4w'
